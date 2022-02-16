2ffeb40be41c49e7e286fe9f23897b93b3c063f1
F3NJ0 <58293913+F3NJ0@users.noreply.github.com>
2022-02-16 11:48:43 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const url = require('url');
+const http = require('http');
+
+http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello Node!\n');
+}).listen(8080);
+
+console.log('My first Node server is running on port 8080.');
