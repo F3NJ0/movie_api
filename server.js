332367797160877e62675461f3a30803685f56ea
F3NJ0 <58293913+F3NJ0@users.noreply.github.com>
2022-02-17 10:45:29 +0100

    Add comments to server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+/* Importing http, url, and fs module */
 const http = require('http'),
   url = require('url'),
   fs = require('fs');
 
+/* Creating http server that listens on port 8080 */
 http.createServer((request, response) => {
+  /* Parsing request url and storing in variable q */
   let addr = request.url;
   q = url.parse(addr, true),
   filePath = '';
 
+  /* Storing request address and timestamp in log.txt file */
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
     if (err) {
       console.log(err);
@@ -15,12 +19,15 @@ http.createServer((request, response) => {
     }
   });
 
+  /* Setting file path to documentation.html file if pathname includes documentation,
+  otherwise to index.html */
   if (q.pathname.includes('documentation')){
     filePath = (__dirname + '/documentation.html');
   } else {
     filePath = 'index.html';
   }
 
+  /* Reading and returning the respective file from filePath */
   fs.readFile(filePath, (err, data) => {
     if (err) {
       throw err;
